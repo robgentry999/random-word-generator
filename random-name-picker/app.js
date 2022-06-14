e68fce528b8db3ab51c772c2598e268fb59847ff
robgentry999 <robertmgentry03@gmail.com>
2022-06-14 02:39:40 -0400

    almost done only problem is nan

--- random-name-picker/app.js
+++ random-name-picker/app.js
@@ -1,6 +1,7 @@
 button = document.getElementById('add-name')
 button2 = document.getElementById('start')
 count = 1
+var words =
 
 //Add Name Function
 button.addEventListener('click', addName);
@@ -12,6 +13,7 @@ button.addEventListener('click', addName);
             newone.type = 'input'
             newone.placeholder = 'Name'
             count++
+            newone.id = count 
 
         var container = 
         document.getElementById('base')
@@ -19,15 +21,34 @@ button.addEventListener('click', addName);
     }
 //Random Number Function
 
-function generateRandomInteger(count) {
-    return Math.floor(Math.random() * count) + 1;
+function generateRandomIntegerInRange(min, count) {
+    return Math.floor(Math.random() * (count - min + 1)) + min;
 }
+
 
 //Start Button
 button2.addEventListener('click' , chooseRandom);
 
-    function chooseRandom(){
+     function chooseRandom(){
+       windiv = document.getElementById('winners-div');
+        const value5 = generateRandomIntegerInRange(1, count);
+        console.log(value5)
+       words = document.getElementById(value5).value
+       console.log(words)
+       windiv.innerHTML =+ `words`
+       console.log(words)
 
-        generateRandomInteger()
-        console.log(generateRandomInteger(count))
+
+    //    console.log(words)
+    //     displayWinner(words)
+    //     return words
     }
+
+
+    // function displayWinner(words) {
+    //    windiv = document.getElementById('winner-div');
+    //     windiv.innerHTML =+ words
+    //     console.log(words)
+    // }
+
+
