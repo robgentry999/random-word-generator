d594934d95193622a1a892b70d8294b42ba2a1fd
robgentry999 <robertmgentry03@gmail.com>
2022-06-14 00:49:11 -0400

    added files and working add names button

---
+++ random-name-picker/app.js
@@ -0,0 +1,16 @@
+button = document.getElementById('add-name')
+console.log(button)
+//Add Name Function
+
+button.addEventListener('click', addName);
+
+function addName(){
+    var newInput = 
+        newone = document.createElement('input')
+        newone.className = 'user-input'
+        newone.type = 'input'
+
+    var container = 
+    document.getElementById('base')
+    container.appendChild(newInput)
+}
---
+++ random-name-picker/readme.md
@@ -0,0 +1,5 @@
+Setup
+
+1. design basic user page
+
+2. Javascript - 
