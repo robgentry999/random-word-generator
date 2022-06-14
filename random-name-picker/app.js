f5ab6c2021b4a353ac30741d0dda6f8b411ef3a4
robgentry999 <robertmgentry03@gmail.com>
2022-06-14 01:23:36 -0400

    random number based on players

--- random-name-picker/app.js
+++ random-name-picker/app.js
@@ -1,16 +1,33 @@
 button = document.getElementById('add-name')
-console.log(button)
-//Add Name Function
+button2 = document.getElementById('start')
+count = 1
 
+//Add Name Function
 button.addEventListener('click', addName);
 
-function addName(){
-    var newInput = 
-        newone = document.createElement('input')
-        newone.className = 'user-input'
-        newone.type = 'input'
+    function addName(){
+        var newInput = 
+            newone = document.createElement('input')
+            newone.className = 'user-input'
+            newone.type = 'input'
+            newone.placeholder = 'Name'
+            count++
 
-    var container = 
-    document.getElementById('base')
-    container.appendChild(newInput)
+        var container = 
+        document.getElementById('base')
+        container.appendChild(newInput)
+    }
+//Random Number Function
+
+function generateRandomInteger(count) {
+    return Math.floor(Math.random() * count) + 1;
 }
+
+//Start Button
+button2.addEventListener('click' , chooseRandom);
+
+    function chooseRandom(){
+
+        generateRandomInteger()
+        console.log(generateRandomInteger(count))
+    }
--- random-name-picker/readme.md
+++ random-name-picker/readme.md
@@ -2,4 +2,7 @@ Setup
 
 1. design basic user page
 
-2. Javascript - 
+2. Javascript -
+    1.create random number gen based on number of players
+
+    2. player corresponding with number is shown 
